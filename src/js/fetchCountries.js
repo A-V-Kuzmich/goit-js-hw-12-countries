220b423dbdb739ceca6c8a846aa00dffa7826ca1
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-10-20 00:13:21 +0300

    update

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,36 +1,56 @@
 //searchQuery
 import countryTp from '../partials/country.hbs';
 import debounce from 'debounce';
+import refs from './refs.js';
+import countryListTP from '../partials/countryList.hbs';
+
+const {main, input, countryList} = refs;
 
 
-const refs = {
-    main: document.querySelector('main'),
-    input: document.querySelector('#input'),
-
-}
-
-refs.input.addEventListener('input', debounce((e) => {
-   console.log(e.target.value);
+input.addEventListener('input', debounce((e) => {
+    console.log(e.target.value);
+    action(e.target.value);
   }, 500)
 );
 
 
-function fetchCountries() {
-    //return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
-    return fetch(`https://restcountries.com/v2/name/peru`)
+
+function action(countryes) {
+    fetchCountry(countryes)
+        .then(result => {
+             if (result.length === 1) {
+                 console.log(result);
+              return  renderCountry(result);
+            }
+            if (result.length < 10) {
+               return renderList(result);
+            }
+            return console.log(`значение ${result.length}`);
+        })
+        .catch(error => console.log(error));
+    
+};
+
+
+function renderList(countries) {
+    // if (countryList) {
+    //     countryList.innerHTML('');
+    // };
+    console.log(countries);
+    const markap = countryListTP(countries);
+    console.log(markap);
+    input.insertAdjacentHTML('afterend', markap);
+ };
+
+
+function fetchCountry(searchQuery) {
+    return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
         .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            const markap = countryTp(data[0]);
-            console.log(markap);
-            refs.main.insertAdjacentHTML('beforeend', markap);
-        })
-        .catch(error => {
-            console.log(error);
+            return response.json()
         });
-    }
+};
 
-fetchCountries();
-
+function renderCountry(country) {
+    const markap = countryTp(country[0]);
+    main.insertAdjacentHTML('beforeend', markap);
+ };
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+  const refs = {
+    main: document.querySelector('main'),
+    countryList: document.querySelector('.country-list'),
+    input: document.querySelector('#input'),
+
+};
+export default refs;
